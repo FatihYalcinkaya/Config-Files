b05204529d9d5b66c7fa7108a2307dad7028c91b
Fatih Yalçınkaya <96224317+FatihYalcinkaya@users.noreply.github.com>
2024-04-12 14:38:19 +0300

    Add files via upload

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -82,9 +82,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = {"rofi","-show","drun","-show-icons",NULL};
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
-
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
